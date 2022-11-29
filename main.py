e5329587fa8f40f058a021adbdf202adda0c153f
andron3a <andron3a@mail.ru>
2022-11-29 20:02:30 +0300

    Написали класс Department

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ class Employee:
         self_bdate, other_bdate = str_to_date(self.bdate, other.bdate)
         return self_bdate < other_bdate
 
-    def __eq__(self, other): #==
+    def __eq__(self, other):  # ==
         self_bdate, other_bdate = str_to_date(self.bdate, other.bdate)
         return self_bdate == other_bdate
 
-    def __le__(self, other): #<=
+    def __le__(self, other):  # <=
         if self.__eq__(other):
             return True
         if self.__lt__(other):
@@ -40,9 +40,37 @@ class Employee:
         return False
 
 
+class Department:
+    def __init__(self, title, chief=None, employees=None):
+        self.title = title
+        if employees is None:
+            employees = list()
+        self.employees = employees
+        self.chief = chief
+
+    def append(self, emp):
+        self.employees.append(emp)
+
+    def __str__(self):
+        return f"Отдел: {self.title}, начальник: {self.chief}, кол-во сотрудников: {len(self.employees)}"
+
+    def print_employees(self):
+        for emp in self.employees:
+            print(emp)
+
+    def print_employees_on_leave (self, status=True):
+        for emp in self.employees:
+            if emp.on_leave == status:
+                print (emp)
 
 petrov = Employee(1, "Петров А.А.", "27.12.1998", 50000)
-smirnoff = Employee(1, "Смирнов П.Р.", "22.04.1999", 46000)
+smirnoff = Employee(2, "Смирнов П.Р.", "22.04.1999", 46000, True)
+kraftsova = Employee(3, "Кравцова Ю.Н.", "17.04.2001", 44000, True)
 print(petrov < smirnoff)
 print(petrov <= smirnoff)
 print(petrov == smirnoff)
+
+arhiv = Department('Архив', employees=[petrov])
+arhiv.append (smirnoff)
+arhiv.append (kraftsova)
+arhiv.print_employees_on_leave(False)
